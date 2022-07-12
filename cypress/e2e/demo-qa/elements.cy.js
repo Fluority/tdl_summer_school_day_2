e30e0b7671b09ebd7ca23a7e6216e43fc46ae3d7
Student17 <Student17@school-7715.local>
2022-07-12 13:57:57 +0300

    first scenario

--- cypress/e2e/demo-qa/elements.cy.js
+++ cypress/e2e/demo-qa/elements.cy.js
@@ -1,6 +1,7 @@
 import TextBoxPage from "../../pageObjects/textBoxPage"; // - finished
 import CheckBoxPage from "../../pageObjects/checkBoxPage"; // - not finished
 import RadioButtonsPage from "../../pageObjects/radioButtonsPage"; // - finished
+import WebTablesPage from "../../pageObjects/webTablesPage"; // - finished
 
 context("Elements Page", () => {
   context("Text box scenarios", () => {
@@ -50,7 +51,7 @@ context("Elements Page", () => {
     beforeEach(() => {
       RadioButtonsPage.visit();
     });
-    it.only("Radio buttons", () => {
+    it("Radio buttons", () => {
       // Scenario 1:
       // Click yesButton
       RadioButtonsPage.yesButton.click();
@@ -67,16 +68,37 @@ context("Elements Page", () => {
 
   context("Web tables scenarios", () => {
     // Create WebTables page object
-    // Create scenario 1:
-    // Click add record button
-    // fill in the necessary information
-    // click submit button
-    // search for the user based on previously added information
-    // validate tha the user is visible
-
-    // Create Scenario 2:
-    // Delete all table rows
-    // Validate that we see text - No rows found
+    beforeEach(() => {
+      WebTablesPage.visit();
+    });
+    it.only("Web tables", () => {
+      // Create scenario 1:
+      // Click add record button
+      WebTablesPage.addRecordButton.click();
+      // fill in the necessary information
+      WebTablesPage.firstName.type("John");
+      WebTablesPage.lastName.type("Doe");
+      WebTablesPage.email.type("johndoe@gmail.com");
+      WebTablesPage.age.type("24");
+      WebTablesPage.salary.type("500");
+      WebTablesPage.department.type("Human Resources");
+      // click submit button
+      WebTablesPage.submit.click();
+      // search for the user based on previously added information
+      WebTablesPage.search.type("johndoe@gmail.com");
+      // validate that the user is visible
+      WebTablesPage.searchVali.should("contain.text","JohnDoe24johndoe@gmail.com500Human Resources"); // 'Contain' instead of 'have' to ignore spaces
+      // Create Scenario 2:
+      WebTablesPage.search.clear();
+      // Delete all table rows
+      // WebTablesPage.delete.click({multiple: true});
+      WebTablesPage.delete1.click();
+      WebTablesPage.delete2.click();
+      WebTablesPage.delete3.click();
+      WebTablesPage.delete4.click();
+      // Validate that we see text - No rows found
+      WebTablesPage.noData.should("contain.text","No rows found");
+    });
   });
 
   context("Buttons scenarios", () => {
---
+++ cypress/pageObjects/webTablesPage.js
@@ -0,0 +1,71 @@
+import BasePage from "./basePage";
+
+class WebTablesPage extends BasePage {
+  static get url() {
+    return "/webtables";
+  }
+
+
+  static get addRecordButton() {
+    return cy.get('#addNewRecordButton');
+  }
+
+  static get firstName() {
+    return cy.get('#firstName');
+  }
+
+  static get lastName() {
+    return cy.get('#lastName');
+  }
+
+  static get email() {
+    return cy.get('#userEmail');
+  }
+
+  static get age() {
+    return cy.get('#age');
+  }
+
+  static get salary() {
+    return cy.get('#salary');
+  }
+
+  static get department() {
+    return cy.get('#department');
+  }
+
+  static get submit() {
+    return cy.get('#submit');
+  }
+
+  static get search() {
+    return cy.get('#searchBox');
+  }
+
+  static get searchVali() {
+    return cy.get('div[class="rt-td"]');
+  }
+
+//   static get delete() {
+//     return cy.get('span[title="Delete"]')
+//   }
+
+  static get delete1() {
+    return cy.get('#delete-record-1')
+  }
+  static get delete2() {
+    return cy.get('#delete-record-2')
+  }
+  static get delete3() {
+    return cy.get('#delete-record-3')
+  }
+  static get delete4() {
+    return cy.get('#delete-record-4')
+  }
+
+  static get noData() {
+    return cy.get('div[class="rt-noData"]');
+  }
+}
+
+export default WebTablesPage;
